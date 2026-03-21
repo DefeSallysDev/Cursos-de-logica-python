6f8cb43fd81bcfe09f88792c137677cef2a46966
DefeSallysDev <deivytorres601@gmail.com>
2026-03-21 02:08:21 -0500

    Add files via upload

---
+++ ejercicio_0.py
@@ -0,0 +1,41 @@
+print ("Instalacion exitosa")
+"""
+comentario sin relevancia 
+en
+multiples lineas
+"""
+'''
+esto tambien es un
+ comentario que no afecta
+ en nada la funcion del codigo 
+en multiples lineas
+'''
+# este tambien pero solo en una linea
+
+My_variable = "creacion de variable"
+My_variable = "puede trasmutar a otra variable dandole un valor nuevo" 
+
+# en python no se puede hacer constantes
+
+MY_CONSTANT = "Pero por convencion s epuede si la variable se pone en mayusculas"
+MY_CONSTANT = "Prueba si python lo lanza con error" # No lanzo error, recomendacion nunca poner 2 constantes con la musma variable
+
+my_int = 1 # dato primitivo que permite representar numeros enteros :v For example 1 , 2 , 3 etc
+
+resultado = my_int + 5
+
+print (resultado);
+
+Variable = 0.245
+
+print (type(Variable)); #comando type sirve para saber como interpreta python el numero (int, float, tuple , cadena de texto y booleano)
+ # resulta que los puntos se interpretan como decimales y las comas como separadores
+
+
+My_bool = True # este dato hace que el valor de la variable sea verdadero o falso, solo esas 2 
+My_bool = False
+
+my_str = "cadena de texto" # representa el texto tal cual
+
+print ("Hola python");
+
